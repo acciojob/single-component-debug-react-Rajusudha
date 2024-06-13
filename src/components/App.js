45ea3e84d305df91de1198a6d889a51bea01c0f1
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2024-06-13 16:40:01 +0000

    nam

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,14 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { Component } from 'react'
+
+export default class Aspp extends Component {
+  render() {
+    return (
+      <div>
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+      </div>
+    )
+  }
+}
+// export default App;
+
 
